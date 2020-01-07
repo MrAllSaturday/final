81bb10d308f853ca099ded8e6db3f4f251e567e6
MrAllSaturday <56427880+MrAllSaturday@users.noreply.github.com>
2020-01-07 00:46:50 -0800

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,24 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(int argc,char*argv[])
+int main(int argc, char *argv[])
 {
     int a = 0;
     double f;
     int c = argv[++a];
-    if ((--argc)>0)
+    while ((--argc) > 0 )
     {   
-        f = atof(argv[++a]);
-        if(c>f);
+        f = atof(argv[a]);
+        if(c > f)
         c = f;
-        a = a + 1;
-        printf("Min parameter is %d\n",c);
+        a++;
     }
-    else if (!isdigit(argv[++a]))
-    {
-        printf("The program needs at least one integer parameter to run!");
-    }
-    else 
-    {
-        printf("The parameter has to be an integer format!");
-    } 
+    printf("Min parameter is %d\n",c);
 }
